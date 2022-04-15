2037694c423130e57493d3ff957724a8e14224ff
MohammadLotfi79 <mohammad.lotfi7913@gmail.com>
2022-04-15 18:17:17 +0430

    removed unused imports.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,4 @@
-import React, { useState, useEffect } from "react";
-
-// API
-import API from "../API";
+import React from "react";
 
 // config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";

c6b7fcec46d1525c678002a2f3c54321c874dbad
MohammadLotfi79 <mohammad.lotfi7913@gmail.com>
2022-04-15 18:05:17 +0430

    final commit

--- src/App.js
+++ src/App.js
@@ -8,20 +8,24 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import Movie from "./components/Movie";
 import NotFound from "./components/NotFound";
+// context
+import UserProvider from "./context";
 
 // styles
 import { GlobalStyle } from "./GlobalStyle";
 
 const App = () => (
   <Router>
-    <Header />
-    <GlobalStyle />
+    <UserProvider>
+      <Header />
+      <GlobalStyle />
 
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/:movieId" element={<Movie />} />
-      <Route path="/*" element={<NotFound />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:movieId" element={<Movie />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+    </UserProvider>
   </Router>
 );
 
